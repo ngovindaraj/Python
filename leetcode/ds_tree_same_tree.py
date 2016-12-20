f0cd6be74ec634a73fd687b0d39d3f0eb391c51b
ngovindaraj <navina.govindaraj@gmail.com>
2016-12-20 09:03:22 -0800

    Tree same tree (file creation)

---
+++ leetcode/ds_tree_same_tree.py
@@ -0,0 +1,12 @@
+# @file  Same Tree
+# @brief Given 2 BT, do they have same structure and values
+
+# https://leetcode.com/problems/same-tree/
+
+'''
+Given two binary trees, write a function to check if they are
+equal or not.
+Two binary trees are considered equal if they are structurally
+identical and the nodes have the same value.
+'''
+
