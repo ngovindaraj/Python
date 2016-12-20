37f67b234d1b60a19f2bd8e391ba6615547da17a
ngovindaraj <navina.govindaraj@gmail.com>
2016-12-20 09:05:48 -0800

    Tree - Same Tree (add implementation)

--- leetcode/ds_tree_same_tree.py
+++ leetcode/ds_tree_same_tree.py
@@ -10,3 +10,11 @@ Two binary trees are considered equal if they are structurally
 identical and the nodes have the same value.
 '''
 
+def isSameTree(self, p, q):
+  if p == None or q == None : return p == q
+  elif p.val != q.val       : return False
+  elif self.isSameTree(p.left, q.left) == False: return False
+  else:                       return self.isSameTree(p.right, q.right)
+        
+# [1, 2, 3], [1, 2, 3],  true
+# [10,5,15], [10,5,null,null,15],   false
