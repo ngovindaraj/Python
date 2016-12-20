51c915b270304c36e1d255d06783ee10544201c5
ngovindaraj <navina.govindaraj@gmail.com>
2016-12-20 11:06:06 -0800

    Leet - string strstr

---
+++ leetcode/ds_string_strstr.py
@@ -0,0 +1,22 @@
+# @file  Strstr
+# @brief Find first occurence of needle in a haystack
+
+# https://leetcode.com/problems/implement-strstr/
+
+'''
+Implement strStr().
+Returns the index of the first occurrence of needle in
+haystack, or -1 if needle is not part of haystack.
+'''
+
+#Approach 1: Brute force 2-loop approach
+  def strStr(self, haystack, needle):
+    for i in range(0, len(haystack) - len(needle) + 1): #stop i early to avoid going beyond index
+      if haystack[i : i + len(needle)] == needle : return i
+    return -1
+        
+# haystack,  needle,       idx
+# "",            "",         0
+# "abc",       "bc",         1
+# "aaaa",     "aaa",         0
+
