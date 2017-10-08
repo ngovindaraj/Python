607c5bc57199178b494308f4f313b2209981729e
Navina Govindaraj <navina.govindaraj@gmail.com>
2017-10-08 13:16:09 -0700

    add list - contains duplicate - 4 solutions

--- leetcode/ds_list_contains_duplicate.py
+++ leetcode/ds_list_contains_duplicate.py
@@ -10,11 +10,39 @@ array, and it should return false if every element is distinct.
 '''
 
 
-# Dictionary approach (time_complexity = O(n), space_complexity = O(n))
-def containsDuplicate(self, nums):
+# Brute force - 2 loop approach (complexity: time = O(n^2), space = O(1))
+# Note: This is not accepted by leetcode because of high time complexity
+def containsDuplicate1(self, nums):
+    if len(nums) < 2:
+        return False
+    for i in range(len(nums)-1):
+        for j in range(i+1, len(nums)):
+            if nums[i] == nums[j]:
+                return True
+    return False
+
+
+# Sort approach (complexity: time = O(n log n), space = O(1))
+def containsDuplicate2(self, nums):
+    if len(nums) < 2:  #if num elements is less than 2, no duplicates
+        return False
+    nums.sort()
+    for i in range(len(nums)-1):
+        if nums[i] == nums[i+1]:
+            return True
+    return False
+
+
+# Dictionary approach (complexity: time = O(n), space = O(n))
+def containsDuplicate3(self, nums):
     dict = {}
     for elem in nums:
         dict[elem] = dict.get(elem, 0) + 1
         if dict[elem] > 1:
             return True
     return False
+
+
+# Set approach  (complexity: time = O(n), space = O(n))
+def containsDuplicate4(self, nums):
+    return len(nums) != len(set(nums))
