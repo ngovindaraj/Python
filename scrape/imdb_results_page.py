21dd47c7681a6f3de6d9def4d405e3428a249eee
Navina Govindaraj <navina.govindaraj@gmail.com>
2017-10-23 11:46:46 -0700

    updating file

--- scrape/imdb_results_page.py
+++ scrape/imdb_results_page.py
@@ -26,7 +26,6 @@ Inside one page:
 import requests
 from bs4 import BeautifulSoup
 
-
 # Get html contents
 def readURL(url):
     # page = requests.get(url)
@@ -34,72 +33,65 @@ def readURL(url):
         data = myfile.read()
     return data
 
-def readURL2(url):
-    # page = requests.get(url)
-    with open(url, 'r') as myfile:
-        data2 = myfile.read()
-    return data2
-
 # url1
 html = readURL('/Users/navina/Desktop/imdb_test.htm')
 soup = BeautifulSoup(html, 'html.parser')
 
 # url2
-html2 = readURL2('/Users/navina/Desktop/movie1.htm')
+html2 = readURL('/Users/navina/Desktop/movie1.htm')
 soup2 = BeautifulSoup(html2, 'html.parser')
-
-
-
-'''
-# attributes from url2
-
-#director = soup2.find("span", "itemprop").find(itemprop="director").get_text()
-#print(director)
-
-total_votes = soup2.find("span", {"itemprop": "ratingCount"}).get_text()
-print(total_votes)
-
-#us_boxoffice_gross = soup2.find("div", "article").get_text()
-#print(us_boxoffice_gross)
-
-stars = soup2.find("span", {"itemprop": "actors"}).get_text()
-print(stars)
-
-box_office = soup2.find("")
-budget = soup2.find("span", {"itemprop": "actors"}).get_text()
-print(budget)
-
-release_date = soup2.find("a", {"title": "See more release dates"}).get_text()
-print(release_date)
-
-writer = soup2.find("span", {"itemprop": "creator"}).get_text()
-print(writer)
-
-opening_weekend_gross = soup2.find("span", {"itemprop": "creator"}).get_text()
-print(opening_weekend_gross)
-
-'''
 
 def process_movie_main_link(mv):
     sno = mv.find("span", "lister-item-index unbold text-primary").get_text()
     title = mv.find("h3", "lister-item-header").find("a").get_text()
     mpaa_rating = mv.find("span", "certificate").get_text()
     metascore = mv.find("span", "metascore").get_text()
-    user_rating = mv.find("div", "inline-block ratings-imdb-rating").get_text()
-    genre = mv.find("span", "genre").get_text()
-    runtime = first_movie.find("span", "runtime").get_text()
-    print("{sn}. {title} ({genre}) has following parameters:".format(
-        sn=sno, title=title))
-    print("   mpaa={mpaa}, rating({critic} {user}), score={score}, runtime={run}"
-    #add to mysql
+    user_rating = mv.find("div", "inline-block ratings-imdb-rating").find("strong").get_text()
+    genre = mv.find("span", "genre").get_text().strip()
+    runtime = mv.find("span", "runtime").get_text()
+    print("Serial No. {}").format(sno)
+    print("Movie Title: {}").format(title)
+    print("MPAA Rating: {}").format(mpaa_rating)
+    print("Metacore: {}").format(metascore)
+    print("User Rating: {}").format(user_rating)
+    print("Genre: {}").format(genre)
+    print("Runtime {}").format(runtime)
 
 
 main_section = soup.find(id="main")
 all_movie_tags = main_section.select(".article .lister-item-content")
-for each movie in all_movie_tags:
-    process_movie_main_link(movie)
-    break
 
+for each_movie in all_movie_tags:
+    process_movie_main_link(each_movie)
+
+
+
+
+    #add to mysql
+
+
+
+
+'''
+# test for first movie
+
+first_movie = all_movie_tags[0]
+print(first_movie)
+sno = first_movie.find("span", "lister-item-index unbold text-primary").get_text()
+title = first_movie.find("h3", "lister-item-header").find("a").get_text()
+mpaa_rating = first_movie.find("span", "certificate").get_text()
+metascore = first_movie.find("span", "metascore").get_text()
+user_rating = first_movie.find("div", "inline-block ratings-imdb-rating").find("strong").get_text()
+genre = first_movie.find("span", "genre").get_text().strip()
+runtime = first_movie.find("span", "runtime").get_text()
+
+print("Serial No. {}".format(sno))
+print("Movie Title: {}".format(title))
+print("MPAA Rating: {}".format(mpaa_rating))
+print("Metacore: {}".format(metascore))
+print("User Rating: {}".format(user_rating))
+print("Genre: {}".format(genre))
+print("Runtime: {}".format(runtime))
 
 #url2
 
@@ -109,7 +101,7 @@ for each movie in all_movie_tags:
 #titlebar_tags = titlebar.select(".div .subtext")
 #first_movie_titlebar = titlebar_tags[0]
 #print(first_movie_titlebar)
-'''
+
 director = soup2.find("span", "itemprop").find(itemprop="director").get_text()
 #print(director)
 
