6cb9d569919ddc3f0b8f851b2fb6ab86e5633d19
Navina Govindaraj <navina.govindaraj@gmail.com>
2017-12-20 11:21:22 -0800

    add approach 2

--- leetcode/ds_string_strstr.py
+++ leetcode/ds_string_strstr.py
@@ -10,12 +10,16 @@ haystack, or -1 if needle is not part of haystack.
 '''
 
 #Approach 1: Brute force 2-loop approach
-  def strStr(self, haystack, needle):
-    for i in range(0, len(haystack) - len(needle) + 1): #stop i early to avoid going beyond index
-      if haystack[i : i + len(needle)] == needle : return i
-    return -1
-        
-#Approach 2: Boyer-Moore
+def strStr(self, haystack, needle):
+  for i in range(0, len(haystack) - len(needle) + 1): #stop i early to avoid going beyond index
+    if haystack[i : i + len(needle)] == needle : return i
+   return -1
+
+# Approach 2: 
+def strStr(self, haystack, needle):
+  return haystack.find(needle)
+  
+#Approach 3: Boyer-Moore
 #Need to learn and implement
 
 # haystack,  needle,       idx
